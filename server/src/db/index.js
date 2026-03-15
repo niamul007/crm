ed9ad3451549b0ff4f03184cc43bdbf84df3e980
niamul007 <am3994566@gmail.com>
2026-03-15 22:28:24 +0600

    adding db connection

---
+++ server/src/db/index.js
@@ -0,0 +1,17 @@
+import pkg from "pg";
+const { Pool } = pkg;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database. you are ready to go! ');
+});
+export default pool;
