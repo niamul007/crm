11ead9a4ea64e568bfe8b35fc3c8f97705785579
niamul007 <am3994566@gmail.com>
2026-03-17 09:51:03 +0600

    adding get and create controller

---
+++ server/src/controllers/clientController.mjs
@@ -0,0 +1,48 @@
+import * as clientService from "../services/clientService.mjs";
+
+export const getClient = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const clients = await clientService.getClient(userId);
+    res.status(200).json({
+      status: "success",
+      data: { clients },
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const createClient = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const {
+      name,
+      email,
+      phone,
+      company,
+      project_name,
+      project_status,
+      deadline,
+      budget,
+    } = req.body;
+
+    const clients = await clientService.createClient(
+      userId,
+      name,
+      email,
+      phone,
+      company,
+      project_name,
+      project_status,
+      deadline,
+      budget,
+    );
+    res.status(201).json({
+      status: "success",
+      data: { clients },
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
