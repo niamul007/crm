764ac50e87acc08492277324de2b6d0fc86d904f
niamul007 <am3994566@gmail.com>
2026-03-17 09:59:16 +0600

    adding getclientbyid

--- server/src/controllers/clientController.mjs
+++ server/src/controllers/clientController.mjs
@@ -1,3 +1,4 @@
+import { use } from "react";
 import * as clientService from "../services/clientService.mjs";
 
 export const getClient = async (req, res) => {
@@ -41,6 +42,21 @@ export const createClient = async (req, res) => {
     res.status(201).json({
       status: "success",
       data: { clients },
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const getClientById = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const clientId = req.params.id;
+
+    const getClients = await clientService.getClientById(clientId,userId);
+    res.status(200).json({
+      status: "success",
+      data: { getClients },
     });
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
