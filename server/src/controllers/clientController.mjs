18bc218082ecee08c04246865fe281035c3d7cec
niamul007 <am3994566@gmail.com>
2026-03-17 10:45:06 +0600

    adding create and del fn

--- server/src/controllers/clientController.mjs
+++ server/src/controllers/clientController.mjs
@@ -53,11 +53,56 @@ export const getClientById = async (req, res) => {
     const userId = req.user.id;
     const clientId = req.params.id;
 
-    const getClients = await clientService.getClientById(clientId,userId);
+    const getClients = await clientService.getClientById(clientId, userId);
     res.status(200).json({
       status: "success",
       data: { getClients },
     });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const updateClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userId = req.user.id;
+    const {
+      name,
+      email,
+      phone,
+      company,
+      project_name,
+      project_status,
+      deadline,
+      budget,
+    } = req.body;
+
+    const clients = await clientService.updateClient(id, userId, {
+      name,
+      email,
+      phone,
+      company,
+      project_name,
+      project_status,
+      deadline,
+      budget,
+    });
+    res.status(200).json({
+      status: "success",
+      data: { clients },
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+export const deleteClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userId = req.user.id;
+    await clientService.deleteClient(id, userId);
+    res.status(204).send();
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
   }
