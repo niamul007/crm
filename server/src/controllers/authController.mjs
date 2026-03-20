86599c5aebedb1b3bc64345206844f387e6f0586
niamul007 <am3994566@gmail.com>
2026-03-20 11:37:42 +0600

    adding login

--- server/src/controllers/authController.mjs
+++ server/src/controllers/authController.mjs
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const register = async (req, res) => {
   try {
     const { email, password, username } = req.body;
-    const user =await authService.registerUser(email, password, username);
+    const user = await authService.registerUser(email, password, username);
 
     const token = jwt.sign(
       { id: user.id },
@@ -31,4 +31,45 @@ export const register = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await authService.findUserByEmail(email);
 
+    if (!user) {
+      return res
+        .status(401)
+        .json({ status: "fail", message: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: "Fail",
+        message: "Password doesn't match",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET || "dev secret key",
+      { expiresIn: "1d" },
+    );
+
+    res.status(200).json({
+      status: "success",
+      token: token,
+      message: "Login succesfull",
+      data: {
+        user: {
+          id: user.id,
+          email: user.email,
+          username: user.username || "New User",
+        },
+      },
+    });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
