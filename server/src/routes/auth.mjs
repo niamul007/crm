22d629ae9cf1b6f03436220541fcaee3cf7ca7bd
niamul007 <am3994566@gmail.com>
2026-03-21 18:27:47 +0600

    adding validation to the routes

--- server/src/routes/auth.mjs
+++ server/src/routes/auth.mjs
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import * as authController from '../controllers/authController.mjs';
+import { validateSchema } from '../validation/validate.mjs';
+import { registerSchema, loginSchema } from '../validation/authValidation.mjs';
 
-router.post("/register",authController.register);
-router.post("/login",authController.login)
+router.post("/register",validateSchema(registerSchema),authController.register);
+router.post("/login",validateSchema(loginSchema),authController.login)
 
 export default router;
