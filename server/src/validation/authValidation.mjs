d2e18c99599838dcfb0635fccb9f2e02815c8bc0
niamul007 <am3994566@gmail.com>
2026-03-21 17:00:10 +0600

    adding z.obj on the body i came to know that it's importantfor zod 4

--- server/src/validation/authValidation.mjs
+++ server/src/validation/authValidation.mjs
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  body: {
+  body: z.object({
     username: z.string().min(3, { message: "username must be 3 character" }),
     email: z.string().email("invalid email"),
     password: z.string().min(3, "password must be at least  character"),
-  },
+  }),
 });
 
 export const loginSchema = z.object({
-  body: {
+  body: z.object({
     email: z.string().email("invalid email"),
     password: z.string().min(3, "password must be at least  character"),
-  },
+  }),
 });
