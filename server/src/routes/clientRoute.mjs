f1e2a32fd76104c1f681b03ae2900a042457df77
niamul007 <am3994566@gmail.com>
2026-03-21 18:28:18 +0600

    adding validation

--- server/src/routes/clientRoute.mjs
+++ server/src/routes/clientRoute.mjs
@@ -2,16 +2,18 @@ import express from 'express';
 import { protect } from '../middleware/authMiddleware.mjs'
 const router = express.Router();
 import * as clientController from "../controllers/clientController.mjs";
+import { validateSchema } from '../validation/validate.mjs';
+import { clientSchema, paymentSchema, noteSchema } from '../validation/clientSchema.mjs';
 
 router.use(protect)
 router.get("/", clientController.getClient);
-router.post("/", clientController.createClient);
+router.post("/",validateSchema(clientSchema), clientController.createClient);
 router.get("/:id", clientController.getClientById);
 router.delete("/:id", clientController.deleteClient);
-router.put("/:id", clientController.updateClient);
-router.post("/:id/payments", clientController.addPayments);
+router.put("/:id",validateSchema(clientSchema), clientController.updateClient);
+router.post("/:id/payments",validateSchema(paymentSchema), clientController.addPayments);
 router.delete("/:id/payments/:pid", clientController.deletePayments)
-router.post("/:id/notes",clientController.addNote);
+router.post("/:id/notes",validateSchema(noteSchema),clientController.addNote);
 router.delete("/:id/notes/:nid", clientController.deleteNote)
 
 export default router;
