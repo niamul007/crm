abb757843ff8607d4cd0821de99c6f990e730ef3
niamul007 <am3994566@gmail.com>
2026-03-24 19:33:25 +0600

    adding proteded routes

--- front-end/client/src/App.jsx
+++ front-end/client/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import ClientDetail from "./pages/ClientDetail";
+// import Dashboard from "./pages/Dashboard";
+// import ClientDetail from "./pages/ClientDetail";
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoutes";
 
 function App() {
   return (
@@ -12,8 +13,22 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/client/:id" element={<ClientDetail />} />
+        {/* <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        /> */}
+        {/* <Route
+          path="/client/:id"
+          element={
+            <ProtectedRoute>
+              <ClientDetail />
+            </ProtectedRoute>
+          }
+        /> */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
