5cd41efb81ecb6c67664b52a623b3d442ca73f47
niamul007 <am3994566@gmail.com>
2026-03-24 19:36:24 +0600

    addng me path

--- server/src/routes/auth.mjs
+++ server/src/routes/auth.mjs
@@ -13,11 +13,12 @@
 import express from 'express';
 const router = express.Router();
 import * as authController from '../controllers/authController.mjs';
+import { getMe } from '../controllers/clientController.mjs';
 import { validateSchema } from '../validation/validate.mjs';
 import { registerSchema, loginSchema } from '../validation/authValidation.mjs';
-
+import { protect } from '../middleware/authMiddleware.mjs';
 router.post("/register", validateSchema(registerSchema), authController.register);
 router.post("/login", validateSchema(loginSchema), authController.login);
-router.get("/me", protect, authController.getMe);
+router.get("/me", protect, getMe);
 
 export default router;
