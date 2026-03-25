89b7b3213365274b9e091ddd7eaa67125eb9398e
niamul007 <am3994566@gmail.com>
2026-03-25 11:13:42 +0600

    uncomment paths

--- front-end/client/src/App.jsx
+++ front-end/client/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-// import Dashboard from "./pages/Dashboard";
-// import ClientDetail from "./pages/ClientDetail";
+import Dashboard from "./pages/Dashboard";
+import ClientDetail from "./pages/ClientDetail";
+import ClientForm from "./components/ClientForm";
+
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoutes";
@@ -13,22 +15,30 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route
+        <Route
           path="/dashboard"
           element={
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
           }
-        /> */}
-        {/* <Route
-          path="/client/:id"
+        />
+        <Route
+          path="/clients/:id"
           element={
             <ProtectedRoute>
               <ClientDetail />
             </ProtectedRoute>
           }
-        /> */}
+        />
+        <Route
+          path="/clients/new"
+          element={
+            <ProtectedRoute>
+              <ClientForm />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
