adbb58c03fe18248df4cc33bbf326a37ba4907dc
niamul007 <am3994566@gmail.com>
2026-03-26 22:34:00 +0600

    adding doc

--- front-end/client/src/api/axios.js
+++ front-end/client/src/api/axios.js
@@ -1,10 +1,19 @@
+/**
+ * axios.js — configured Axios instance
+ * 
+ * - baseURL pulled from .env (VITE_API_BASE_URL)
+ * - Request interceptor automatically attaches JWT token
+ *   to every request header so we don't repeat it manually
+ *   in every API call
+ */
+
 import axios from "axios";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
 })
 
-
+// Add a request interceptor to include the token in all requests
 API.interceptors.request.use((config)=>{
 
     const token = localStorage.getItem("token");
